3af6c684d96ab37c23e1875e31fff29dd62092dc
ruslan2020202 <ruslan020507@gmail.com>
2024-03-27 12:31:22 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const images = ['angel.png', 'grimasa.png', 'include.png', 'hungry.png', 'mat.png']
+
+const btnSmile = document.querySelector('.btn-smile')
+const smileImg = document.querySelector('.smileImg') 
+let num = 0
+let smileWidth = smileImg.clientWidth  = '160px'
+let smileHeigth = smileImg.clientHeight = '160px'
+
+let timer = document.querySelector('.timer')
+
+btnSmile.addEventListener('click', ()=>{
+    num = num + 1
+    console.log(num)
+    smileHeigth = `${parseInt(smileHeigth) + 2}px`
+    smileWidth = `${parseInt(smileWidth) + 2}px`
+    smileImg.style.width = smileWidth; 
+    smileImg.style.height = smileHeigth; 
+    if (num % 10 == 0){
+        let newImg = num / 10
+        smileImg.src = `../images/${images[newImg]}`
+        smileHeigth = '160px'
+        smileWidth = '160px'
+    }
+    if(num === 49){
+        alert('You win!')
+        window.location.href = '../templates/index.html'
+    }
+    
+})
+
+endTime = Number(String(Date.now()).slice(0, 10)) + 25
+
+function updaterTime() {
+    let currentTime = Number(String(Date.now()).slice(0, 10))
+    let remainingTime = endTime - currentTime
+    if (remainingTime <= 0) {
+        clearInterval(timerInterval)
+        if(num < 49){
+            alert('You lose!')
+            window.location.href = '../templates/index.html'
+        }
+    } else {
+        let minutes = Math.floor(remainingTime / 60)
+        let seconds = remainingTime % 60
+        if (minutes < 10) {
+            minutes = `0${minutes}`
+        }
+        if (seconds < 10) {
+            seconds = `0${seconds}`
+        }
+        timer.textContent = `${minutes}:${seconds}`
+    }
+}
+
+
+let timerInterval = setInterval(updaterTime, 1000)
+updaterTime()
